31132a15ec7e91b4ad9458cbb98c4ab3b807c17c
ShubhamHsbc17 <luckyluckyjain17@gmail.com>
2025-02-07 14:16:09 +0530

    GetLearner added

--- src/main/java/com/example/EmployeeMS/Controller/GetLearner.java
+++ src/main/java/com/example/EmployeeMS/Controller/GetLearner.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GetLearner {
-    @GetMapping("/")
+    @GetMapping("/learners")
     public String helloWorld()
     {
         return "Hello World";
